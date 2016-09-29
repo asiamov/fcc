af504b371dc8be6670a9b044ed7b9ea85a61316f
Alx <c9@alexander.siamov.org>
2016-09-29 14:56:52 +0000

    first test

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-var express = require('express')
+var url = require('url')
+var http = require('http')
+
+var isDate = function(date) {
+    return ((new Date(date)).toString() !== "Invalid Date") ? true : false;         
+}
+
+var server = http.createServer(function (req, res) {  
+  // request handling logic...  
+    var parsed =  url.parse(req.url, true)
+    console.log(parsed)
+    if (parsed.pathname === "/asd") {
+        if (parsed.query) {
+            console.log(parsed.query)
+            if (isDate(parsed.query)) {
+               var datevar = new Date(parsed.query)
+               res.writeHead(200, { 'Content-Type': 'application/json' })
+               var resJson = {unix: datevar.getTime(),
+                             natural: datevar.getMonth() + datevar.getDay() + datevar.getYear()
+               }
+               console.log(resJson)
+               res.end(JSON.stringify(resJson))
+            }
+            
+            
+            
+        }
+    }
+})  
+server.listen(8080)
